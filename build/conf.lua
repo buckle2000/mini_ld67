225243c68a9f55777f487e8ae07277686f6f8405
buckle2000 <buckle2000@163.com>
2016-05-26 13:36:06 +0800

    modified readme.md, attribute font

--- build/conf.lua
+++
@@ -1,5 +0,0 @@
-function love.conf(t)
-    t.version = "0.10.1"                -- The LÖVE version this game was made for (string)
-    t.window.title = "Rectify"         -- The window title (string)
-    t.window.highdpi = true
-end
